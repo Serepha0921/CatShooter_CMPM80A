a7ec9ca67e9197ae94698109094f3b1b079e96dc
Serepha0921 <jaynumber001@gmail.com>
2022-02-24 16:02:33 -0800

    Changed the sprite of shooter

--- Assets/Scripts/Plyaer_movement.cs
+++ Assets/Scripts/Plyaer_movement.cs
@@ -8,6 +8,9 @@ public class Plyaer_movement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector3 mousePosition;
     private Transform player;
+
+    public GameObject Cat;
+
     [Header("Guns")]
     public Transform gun;
     public GameObject bullet;
@@ -22,10 +25,16 @@ public class Plyaer_movement : MonoBehaviour
     }
 
     private void Update() {
+        /*
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - player.position;
         float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        player.rotation = Quaternion.Slerp(player.rotation, rotation, rotation_speed * Time.deltaTime);
+        player.rotation = Quaternion.Slerp(player.rotation, rotation, rotation_speed * Time.deltaTime);*/
+
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Cat.transform.position;
+        float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Cat.transform.rotation = Quaternion.Slerp(Cat.transform.rotation, rotation, rotation_speed * Time.deltaTime);
 
         if(Input.GetMouseButtonDown(0)){// shoot
             speed = 0f;
@@ -34,7 +43,7 @@ public class Plyaer_movement : MonoBehaviour
             speed = 0f;
         }else{//Move
             speed = Movement_speed;
-            player.position += transform.right * speed *Time.deltaTime;
+            player.position += Cat.transform.right * speed *Time.deltaTime;
         }
     }
 
