20584bc6f225a8456a6da30f44be7ea8b3012321
Serepha0921 <jaynumber001@gmail.com>
2022-02-25 16:56:50 -0800

    Add Explosion For Missile

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,23 +7,34 @@ public class Bullet : MonoBehaviour
     public float Bullet_speed = 20f;
     public int damage = 40;
     public Rigidbody2D rb;
+    [Header("animaiton")]
+    public GameObject explosion;
+    public Transform Pos;
     // Start is called before the first frame update
     private void Start() {
         rb.velocity = transform.right * Bullet_speed;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+
         if(other.tag == "Enemy"){
             Enemy enmy = other.GetComponent<Enemy>();
             if(enmy != null){
                 enmy.GetDamage(damage);
             }
 
-            Destroy(gameObject);
+            anim();
         }
         if(other.tag == "Building"){
-            Destroy(gameObject);
+            anim();
         }
 
     }
+
+    private void anim(){
+        rb.velocity = transform.right * 0;
+        Instantiate(explosion,Pos.position,Quaternion.identity);
+        Destroy(gameObject);
+    } 
+
 }
